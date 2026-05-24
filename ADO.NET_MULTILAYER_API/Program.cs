9345870b655a81b0a36c479a6ef3ac306cef053d
venkat <dell@DESKTOP-S8CP3CH>
2026-05-24 13:58:07 +0530

    userstory-1234560-automapper implemented in servicelayer

--- ADO.NET_MULTILAYER_API/Program.cs
+++ ADO.NET_MULTILAYER_API/Program.cs
@@ -17,6 +17,10 @@ builder.Services.AddSingleton<IConnectionFactory, ConnectionFactory>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 //=====================================================================
+#region AutoMapper Adding To DependencyInjection Container
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
